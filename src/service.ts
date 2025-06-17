f313134dcdbe1d963aba3e1f4ddeb0699715dcf4
Ian Torres <iantorres@outlook.com>
2025-06-17 12:05:46 -0400

    Unused import removed.

--- src/service.ts
+++ src/service.ts
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import { Configuration, Request, RequestType, Response } from './types';
+import { Configuration, Request, Response } from './types';
 import { Connection } from './connection';
 
 /**
